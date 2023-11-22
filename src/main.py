72f36596194475b38843eb261c873432b9b3a7d7
YamAlmog <yamalmog14@gmail.com>
2023-11-23 01:10:28 +0200

    fix @(decorator) for app

--- src/main.py
+++ src/main.py
@@ -12,7 +12,7 @@ alert_aggregator = AlertsAggregator(dataframe)
 
 
 
-app.get("/orefalerts")
+@app.post("/orefalerts")
 async def get_oref_alert(from_date: str, to_date: str):
     try:
         getdata.get_oref_alarms(from_date, to_date)
@@ -21,7 +21,7 @@ async def get_oref_alert(from_date: str, to_date: str):
         raise HTTPException(status_code=404, detail=str(e))
 
 
-app.get("/best_time_to_shower")
+@app.get("/best_time_to_shower")
 async def get_best_time_to_shower(settlement: str, start_time: str, end_time: str):
     try:
         best_time = alert_aggregator.best_time_to_shower(settlement, start_time, end_time)
@@ -30,7 +30,7 @@ async def get_best_time_to_shower(settlement: str, start_time: str, end_time: st
         raise ValueError(status_code=400, detail=str(e))
     
 
-app.get("/worst_time_to_shower")
+@app.get("/worst_time_to_shower")
 async def get_worst_time_to_shower(settlement: str, start_time: str, end_time: str):
     try:
         worst_time = alert_aggregator.worst_time_to_shower(settlement, start_time, end_time)
@@ -39,7 +39,7 @@ async def get_worst_time_to_shower(settlement: str, start_time: str, end_time: s
         raise ValueError(status_code=400, detail=str(e))
     
 
-app.get(":/alerts_count")
+@app.get("/alerts_count")
 async def get_alerts_count(settlement: str):
     try:
         alert_amount = alert_aggregator.alerts_count(settlement)
@@ -47,11 +47,19 @@ async def get_alerts_count(settlement: str):
     except ValueError as e:
         raise ValueError(status_code=400, detail=str(e))
     
-app.get("/total_alerts_count")
+@app.get("/total_alerts_count")
 async def get_total_alerts_count():
     return alert_aggregator.total_alerts_count()
 
 
-app.get("/poorest_city")
+@app.get("/poorest_city")
 async def get_poorest_city():
     return alert_aggregator.poorest_city()
+
+
+@app.get("/get_distribution")
+async def get_distribution(settlement: str):
+    try:
+        return alert_aggregator.display_dist(settlement)
+    except ValueError as e:
+        raise ValueError(status_code=400, detail=str(e))
