722ef63f6f8877ebc8fccae34518c16151ccd14c
YamAlmog <yamalmog14@gmail.com>
2023-11-28 17:02:00 +0200

    handle case of input part of an existing settlement & fix the function create_adjusted_time_column
    Handle in case where a user input part of an existing settlement & use new function- create_15_minute_interval_time_lst  to fix the function of adjusting the time column by quarter of an hour so the bestq worst time to shower will be in format %H:%M.

--- src/AlertsAggregator.py
+++ src/AlertsAggregator.py
@@ -4,7 +4,8 @@ import warnings
 warnings.filterwarnings('ignore')
 from datetime import datetime
 from models import AlertCountPerDay
-from datetime import time, datetime
+from Errors import WrongSettlementException
+from datetime import time, datetime, timedelta
 import requests
 GEONAMES_URL = "http://api.geonames.org/searchJSON"
 
@@ -25,11 +26,14 @@ class AlertsAggregator:
         given_lst = self.return_data_list()
         settl_lst = []
         for item in given_lst:
-            if f"{settlement.lower()} " in item or f"{settlement.lower()}, " in item or f" {settlement.lower()}" in item or settlement.lower() == item:
+            if settlement.lower() == item or f"{settlement.lower()}, " in item:
+                return [item]
+            elif f"{settlement.lower()} " in item  or f" {settlement.lower()}" in item or settlement.lower() in item :
                 if item not in settl_lst:
                     settl_lst.append(item)
-        return settl_lst
-    
+        raise WrongSettlementException(f"Incorrect settlement, please try again input one of this options: {settl_lst}")
+       
+
     
     # This function use geonames api and return True if the user settlement input is actually exist else returns False
     def does_the_settlement_exist(self, settlement: str):
@@ -55,8 +59,8 @@ class AlertsAggregator:
 
     # This function display the distribution of alarms in a specific settlement
     def display_dist(self, settlement: str):
-        settlement_lst= self.create_user_settl_lst(settlement)
         try:
+            settlement_lst= self.create_user_settl_lst(settlement)
             is_settlement_exist = self.does_the_settlement_exist(settlement)
             if settlement_lst != []:
                 # Filtered the df by the given settlement lst
@@ -79,11 +83,15 @@ class AlertsAggregator:
                 raise ValueError("You selected a Settlement that does not exist.")
         
         except requests.exceptions.RequestException as req_err:
-            raise requests.exceptions.RequestException(f"Request error occurred: {req_err}")
+            raise requests.exceptions.RequestException(f"Error occurred: {req_err}")
+        
+        except WrongSettlementException as settl_exc:
+            raise WrongSettlementException(settl_exc)
         
         except ValueError as val_err:
-            raise ValueError(f"ValueError error occurred:{val_err}")    
-
+            raise ValueError(val_err)   
+        
+        
 
     # This function receives df and settlement name then count the total amount of alerts in this settlement
     def alerts_count(self, settlement: str):
@@ -103,75 +111,102 @@ class AlertsAggregator:
                 raise ValueError("You selected a Settlement that does not exist.")
         
         except requests.exceptions.RequestException as req_err:
-            raise requests.exceptions.RequestException(f"Request error occurred: {req_err}")
+            raise requests.exceptions.RequestException(f"Error occurred: {req_err}")
         
+        except WrongSettlementException as settl_exc:
+            raise WrongSettlementException(settl_exc)
+
         except ValueError as val_err:
-            raise ValueError(f"ValueError error occurred:{val_err}")
+            raise ValueError(val_err)
 
         
-
     # This function receives df and count the total amount of alerts in the requested date range
     def total_alerts_count(self):
         total_alerts = self.df.shape[0]
         return {"message" : f"The total amount of alerts in our country is: {total_alerts}"}
     
-   
+    def create_15_minute_interval_time_lst(self, start_time : time, end_time :time):
+        time_list = []
+        current_time = time(start_time.hour, 0)
+        print(current_time)
+        while current_time <= end_time:
+            time_list.append(current_time.strftime('%H:%M'))
+            current_time = (datetime.combine(datetime.today(), current_time) + timedelta(minutes=15)).time()
 
+        return time_list
+    
     # This function receives city, and range of time and returns a df distribution of the alarms in quarters of an hour 0(0-15) 1(15-30) 2(30-45) 3(45-0)
-    def create_quarter_hour_column(self, settlement :str, start_time : time, end_time :time):
-        settlement_lst= self.create_user_settl_lst(settlement)
-        is_settlement_exist = self.does_the_settlement_exist(settlement)
-        # Extract the hour
-        start_hour = start_time.hour
-        end_hour = end_time.hour
+    def create_adjusted_time_column(self, settlement :str, start_time : time, end_time :time):
+        try:    
+            settlement_lst= self.create_user_settl_lst(settlement)
+            is_settlement_exist = self.does_the_settlement_exist(settlement)
+            # Extract the hour
+            start_hour = start_time.hour
+            end_hour = end_time.hour
 
-        if settlement_lst != []:
-            filtered_df = self.df[self.df['data'].isin(settlement_lst)]
-            filtered_df['time'] = pd.to_datetime(filtered_df['time']) 
-            # Filter the deteframe by the start and end time of the user
-            filtered_df = filtered_df[(filtered_df['time'].dt.hour >= start_hour) & (filtered_df['time'].dt.hour < end_hour)]
-            # Create new column which represent the time of the alarm among 15 minuts 
-            filtered_df['quarter_hour'] = filtered_df['time'].dt.minute // 15
-            quarter_hour_counts = filtered_df['quarter_hour'].value_counts().reindex([0,1,2,3], fill_value=0)
-            return quarter_hour_counts
-        elif is_settlement_exist:
-            return {"message" : f"There were no alarms in the settlement: {settlement}"}
-        else:
-            raise ValueError("You selected a Settlement that does not exist.")
+            if settlement_lst != []:
+                filtered_df = self.df[self.df['data'].isin(settlement_lst)]
+                filtered_df['time'] = pd.to_datetime(filtered_df['time']) 
+                # Filter the deteframe by the start and end time of the user
+                filtered_df = filtered_df[(filtered_df['time'].dt.hour >= start_hour) & (filtered_df['time'].dt.hour < end_hour)]
+                
+                # Create new column which represent the minutes by the nearest 15-minute interval
+                filtered_df['adjusted_time'] = filtered_df['time'].dt.strftime('%H:%M')
+                filtered_df['adjusted_time'] = pd.to_datetime(filtered_df['adjusted_time']).dt.round('15min').dt.strftime('%H:%M')
+                print(filtered_df)
+                
+                time_list = self.create_15_minute_interval_time_lst(start_time, end_time)
+                print(time_list)
+                adjusted_time_counts = filtered_df['adjusted_time'].value_counts().reindex(time_list, fill_value=0)
+                print(adjusted_time_counts)
+                return adjusted_time_counts
+            elif is_settlement_exist:
+                return {"message" : f"There were no alarms in the settlement: {settlement}"}
+            else:
+                raise ValueError("You selected a Settlement that does not exist.")
+        
+        except requests.exceptions.RequestException as req_err:
+            raise requests.exceptions.RequestException(f"Error occurred: {req_err}")
+        
+        except WrongSettlementException as settl_exc:
+            raise WrongSettlementException(settl_exc)
+
+        except ValueError as val_err:
+            raise ValueError(val_err)
 
     # This function use create_quarter_hour_column func to return the best quarter of an hour to shower in
     def best_time_to_shower(self, settlement : str, start_time : time, end_time : time):
         try:    
-            quarter_hour_counts = self.create_quarter_hour_column(settlement, start_time, end_time)
-            print(f"{settlement} \n{quarter_hour_counts}")
-            if isinstance(quarter_hour_counts, pd.Series):
+            adjusted_time_counts = self.create_adjusted_time_column(settlement, start_time, end_time)
+            print(f"{settlement} \n{adjusted_time_counts}")
+            if isinstance(adjusted_time_counts, pd.Series):
                 # Detect the quarter of an hour that appeared the less
-                best_time = quarter_hour_counts.idxmin()
+                best_time = adjusted_time_counts.idxmin()
                 return {"mesage" : f"The best time to take a shower is at the quarter: {best_time}"}
             else:
-                return quarter_hour_counts
+                return adjusted_time_counts
         except ValueError as e:
-            raise ValueError(f"Error: {e}")
+            raise ValueError(e)
         except requests.exceptions.RequestException as req_err:
-            raise requests.exceptions.RequestException(f"Request error occurred: {req_err}")
+            raise requests.exceptions.RequestException(f"Error occurred: {req_err}")
         except Exception as ex:
             raise Exception(f"Error: {ex}")
     
     # This function use create_quarter_hour_column func to return the worst quarter of an hour to shower in
     def worst_time_to_shower(self, settlement: str, start_time: time, end_time: time):
         try:    
-            quarter_hour_counts = self.create_quarter_hour_column(settlement, start_time, end_time)
-            print(f"{settlement} \n{quarter_hour_counts}")
-            if isinstance(quarter_hour_counts, pd.Series):
+            adjusted_time_counts = self.create_adjusted_time_column(settlement, start_time, end_time)
+            print(f"{settlement} \n{adjusted_time_counts}")
+            if isinstance(adjusted_time_counts, pd.Series):
                 # Detect the quarter of an hour that appeared the less
-                worst_time = quarter_hour_counts.idxmax()
+                worst_time = adjusted_time_counts.idxmax()
                 return {"mesage" : f"The worst time to take a shower is at the quarter: {worst_time}"}
             else:
-                return quarter_hour_counts
+                return adjusted_time_counts
         except ValueError as e:
-            raise ValueError(f"Error: {e}")
+            raise ValueError(e)
         except requests.exceptions.RequestException as req_err:
-            raise requests.exceptions.RequestException(f"Request error occurred: {req_err}")
+            raise requests.exceptions.RequestException(f"Error occurred: {req_err}")
         except Exception as ex:
             raise Exception(f"Error: {ex}")
 
